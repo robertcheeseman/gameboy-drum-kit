66dc0159c18a2914f702f67a4cea0ddbc41b59e5
Robert C <67304122+robertcheeseman@users.noreply.github.com>
2022-11-01 21:21:55 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const removeTransition = (e) => {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+const playSound = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return;
+    console.log(key)
+    key.classList.add('playing')
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
